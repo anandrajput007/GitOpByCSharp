cc5c0a8ab5c27e4ef8a4740757179b44baac5dac
Anand Rajput <anandrajput007@gmail.com>
2022-05-02 22:36:47 +0530

    Get all branch name by C#

---
+++ GitOpByCSharp/GitOperations.cs
@@ -0,0 +1,34 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitOpByCSharp
+{
+    public static class GitOperations
+    {
+        public static List<string> GetAllBranchName(string localPath)
+        {
+            List<string> lstBranch = new List<string>();
+            try
+            {
+                using (var repo = new Repository(localPath))
+                {
+                    var branches = repo.Branches;
+                    foreach (var b in branches)
+                    {
+                        lstBranch.Add(b.FriendlyName);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return lstBranch;
+        }
+    }
+}
---
+++ GitOpByCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitOpByCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string localPath = @"F:\MYGit\ARTypescript";
+            List<string> lstBranchs = GitOperations.GetAllBranchName(localPath);
+            foreach (var branch in lstBranchs)
+            {
+                Console.WriteLine(branch);
+            }
+            Console.ReadLine();
+        }
+    }
+}
